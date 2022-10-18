af37073c16f6943209e4658521adf70e196f0b37
Veronika <tokareva_v@inbox.ru>
2022-10-18 15:20:25 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+void Palindrome (int x)
+{
+    if (x / 10000 == x % 10 $$ (x / 1000)% 10 ==(x/10)%10)
+    {
+        Console.Writeline("да")$
+    }
+    else
+    {
+        Console.Writeline("нет");
+    }
+}
+
+Console.Clear();
+Console.Write("Введите пятизначное число:");
+int x = int.Parse(Console.ReadLine()!);
+Palindrome(x);
